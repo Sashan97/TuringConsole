bd51be2bb5377da7ee3df1aa00c9ce79aae173b0
Aleksandr <sasharukozhop@gmail.com>
2020-09-08 14:31:19 +0300

    Started main menu implementation

--- Turing Emulator/Program.cs
+++ Turing Emulator/Program.cs
@@ -6,7 +6,49 @@ namespace Turing_Emulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartupMenu();
+        }
+
+        private static void StartupMenu()
+        {
+            bool MenuLoop = true;
+
+            while (MenuLoop) {
+                Console.Clear();
+
+                Console.WriteLine("TURING MACHINE SIMULATOR");
+                Console.WriteLine("1 - Choose code file");
+                Console.WriteLine("2 - Start simulation");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Exit");
+                Console.WriteLine("Take your pick [1-4]: ");
+
+                char item = Console.ReadKey().KeyChar;
+
+                if (item == '1') ChooseFile();
+                else if (item == '2') Simulation();
+                else if (item == '3') OptionsMenu();
+                else if (item == '4') MenuLoop = false;
+                else continue;
+            }
+        }
+
+        private static void OptionsMenu()
+        {
+            Console.WriteLine("Options");
+            Console.ReadKey();
+        }
+
+        private static void Simulation()
+        {
+            Console.WriteLine("Sumulation");
+            Console.ReadKey();
+        }
+
+        private static void ChooseFile()
+        {
+            Console.WriteLine("Choose file");
+            Console.ReadKey();
         }
     }
 }
