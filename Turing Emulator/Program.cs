1e56bf3922b2744a58bebf39d58393a568653d81
Aleksandr <sasharukozhop@gmail.com>
2020-09-10 20:17:23 +0300

    Implemented file reading and syntax check

--- Turing Emulator/Program.cs
+++ Turing Emulator/Program.cs
@@ -6,15 +6,31 @@ namespace Turing_Emulator
 {
     class Program
     {
-        private static readonly string _firstPath = "code1.txt";
-        private static readonly string _secondPath = "code2.txt";
-        private static readonly string _thirdPath = "code3.txt";
+        struct CodeLine
+        {
+            internal string state, newState;
+            internal char symbol, newSymbol;
+            internal byte direction;
+        }
+
+        private const string FILENAME = @"C:\Users\sasho\Documents\TextTestLocation\TextFile1.txt";
+
+        private const int INITIAL_TAPE_LINE = 1;
+        private const int INITIAL_POSITION_LINE = 2;
+        private const int EXPECTED_ARGUMENT_COUNT = 5;
+
+        private static string _initialTape;
+        private static int _initialPosition;
+
+        //private static readonly string _firstPath = "code1.txt";
+        //private static readonly string _secondPath = "code2.txt";
+        //private static readonly string _thirdPath = "code3.txt";
 
         private static int _simulationSpeed = 1;
 
-        private static List<EntryLine> _lines = new List<EntryLine>();
+        private static List<CodeLine> _codeList = new List<CodeLine>();
 
-        static void Main(string[] args)
+        static void Main()
         {
             StartupMenu();
         }
@@ -61,6 +77,17 @@ namespace Turing_Emulator
                 else continue;
             }
         }
+        
+        private static void ChooseFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Currently you cannot change the default file locations and names, which are:");
+            //Console.WriteLine(_firstPath);
+            //Console.WriteLine(_secondPath);
+            //Console.WriteLine(_thirdPath);
+            Console.WriteLine("Press any key to return.");
+            Console.ReadKey();
+        }
 
         private static void ChangeSimultionSpeed()
         {
@@ -84,27 +111,85 @@ namespace Turing_Emulator
             
         }
 
-        struct EntryLine
-        {
-            internal bool direction;
-            internal string state, newState;
-            internal char symbol, newSymbol;
-        }
 
         private static void Simulation()
         {
-            
-        }
-
-        private static void ChooseFile()
-        {
             Console.Clear();
-            Console.WriteLine("Currently you cannot change the default file locations and names, which are:");
-            Console.WriteLine(_firstPath);
-            Console.WriteLine(_secondPath);
-            Console.WriteLine(_thirdPath);
-            Console.WriteLine("Press any key to return.");
+            ReadFile();
+
+            Console.WriteLine(_initialTape);
+            Console.WriteLine(_initialPosition);
+
+            foreach (CodeLine item in _codeList)
+            {
+                Console.WriteLine(item.state + " "  + item.symbol + " " + item.newSymbol + " " + item.direction.ToString() + " " + item.newState);
+            }
+
             Console.ReadKey();
         }
+
+        private static void ReadFile()
+        {
+            IEnumerable<string> lines = System.IO.File.ReadLines(FILENAME);
+            int lineNumber = 0;
+
+            foreach (string line in lines)
+            {
+                if (String.IsNullOrEmpty(line)) continue;
+                lineNumber++;
+                if (lineNumber == INITIAL_TAPE_LINE) _initialTape = line;
+                else if (lineNumber == INITIAL_POSITION_LINE) _initialPosition = int.Parse(line);
+                else
+                {
+
+                    string[] words = line.Split(" ");
+
+                    if (words.Length != EXPECTED_ARGUMENT_COUNT)
+                    {
+                        Console.WriteLine("Syntax error at line " + lineNumber + ". Invalid argument number.");
+                        break;
+                    }
+
+                    CodeLine myLine;
+
+                    myLine.state = words[0];
+
+                    if (char.TryParse(words[1], out char sym)) myLine.symbol = sym;
+                    else
+                    {
+                        Console.WriteLine("Syntax error at line " + lineNumber + ". Symbol is unsupported or provided incorrecty.");
+                        break;
+                    }
+
+                    if (char.TryParse(words[2], out char newSym)) myLine.newSymbol = newSym;
+                    else
+                    {
+                        Console.WriteLine("Syntax error at line " + lineNumber + ". New symbol is unsupported or provided incorrecty.");
+                        break;
+                    }
+
+                    if (char.TryParse(words[3], out char dir))
+                    {
+                        if (dir == 'L') myLine.direction = 0;
+                        else if (dir == 'R') myLine.direction = 1;
+                        else
+                        {
+                            Console.WriteLine("Syntax error at line " + lineNumber + ". Direction can be either L or R.");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Syntax error at line " + lineNumber + ". Direction can be either L or R.");
+                        break;
+                    }
+
+                    myLine.newState = words[4];
+
+                    _codeList.Add(myLine);
+                }
+            }
+        }
+
     }
 }
